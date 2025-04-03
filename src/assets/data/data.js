ebcbee92a5f65e89c4a5852ab1fa157e56ed42ba
ukondang <niaarsuga@gmail.com>
2025-04-03 21:41:47 +0800

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -89,7 +89,7 @@ export const data = {
 
     audio: './src/assets/audio/wedding.mp3',
 
-    api: 'https://script.google.com/macros/s/AKfycbyydz6N4p2VWUG8zsXeURv6ap9RP8a4eC3x6N3x6qTDjMVr1cIBz9S0NsHw2rWvBOSXGg/exec',
+    api: 'https://script.google.com/macros/s/AKfycbyDtefQZgtO2MofxBYjiDQl0N3R25IUcMFqDfyaHB5PfD5JuIyna58e6WO7-cx2HOg17g/exec',
 
     navbar: [
         {
